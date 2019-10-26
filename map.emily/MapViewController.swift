d404475dc099e2b96736ce8872218dd8d5699d11
appleEmily <emiri3128@gmail.com>
2019-10-27 01:12:44 +0900

    地図の使い方。むずい

--- map.emily/MapViewController.swift
+++ map.emily/MapViewController.swift
@@ -7,24 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    let annotation = MKPointAnnotation()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //annotation.coordinate = CLLocationCoordinate2DMake(35.6591, 139.7006)
         // Do any additional setup after loading the view.
+        
+        let center = CLLocationCoordinate2DMake(35.6591, 139.7006)
+        let span : MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated:true)
+        
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- map.emily/listViewController.swift
+++ map.emily/listViewController.swift
@@ -16,15 +16,9 @@ class listViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
 
 }
